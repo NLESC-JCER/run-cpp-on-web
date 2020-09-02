aadfa2b76fc4d2c4acbc1940e8d4cb853c4622ca
cwmeijer <c.meijer@esciencecenter.nl>
2020-09-02 15:58:59 +0200

    simplified sleep in slow js-webapp-async

--- js-webapp-async/newtonraphson_slow.cpp
+++ js-webapp-async/newtonraphson_slow.cpp
@@ -1,34 +1,11 @@
 #include "newtonraphson.hpp"
 #include "algebra.cpp"
 #include <math.h>
-#include <iostream>
-#include <chrono>
-//#include <windows.h>
-#include <stdlib.h>
-#ifdef WIN32
-#include <windows.h>
-#elif _POSIX_C_SOURCE >= 199309L
-#include <time.h>   // for nanosleep
-#else
-#include <unistd.h> // for usleep
-#endif
+#include <unistd.h> // for sleep
 
 using namespace algebra;
 
 namespace rootfinding {
-  void sleep_ms(int milliseconds) // cross-platform sleep function
-  {
-  #ifdef WIN32
-      Sleep(milliseconds);
-  #elif _POSIX_C_SOURCE >= 199309L
-      struct timespec ts;
-      ts.tv_sec = milliseconds / 1000;
-      ts.tv_nsec = (milliseconds % 1000) * 1000000;
-      nanosleep(&ts, NULL);
-  #else
-      usleep(milliseconds * 1000);
-  #endif
-  }
    // Define the constructor method of NewtonRaphson instances
    NewtonRaphson::NewtonRaphson(double tolerancein) : tolerance(tolerancein) {}
 
@@ -41,7 +18,9 @@ namespace rootfinding {
          delta_x = equation(x) / derivative(x);
          x = x - delta_x;
       }
-      sleep_ms(5000);
+
+      // Make this code slow artificially
+      sleep(5);
       return x;
    };
 }
