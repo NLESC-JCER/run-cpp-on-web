750106064c9e1f05e6d4b1fb472148a590c9c8ff
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-10 17:40:11 +0200

    initialized with 0.0 instead of 0

--- run-your-c++-code-on-the-web/newtonraphson.cpp
+++ run-your-c++-code-on-the-web/newtonraphson.cpp
@@ -8,7 +8,7 @@ NewtonRaphson::NewtonRaphson(double tolerance_in) : tolerance(tolerance_in) {}
 // Define the 'solve' method of NewtonRaphson instances
 double NewtonRaphson::solve(double initial_guess) {
   double x = initial_guess;
-  double delta_x = 0;
+  double delta_x = 0.0;
   do {
     delta_x = equation(x) / derivative(x);
     x = x - delta_x;
