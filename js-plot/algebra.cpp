70c5cbc76c633dace410b8f6692f04d960c02b43
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-10 18:36:00 +0200

    Revert "updated equation"
    This reverts commit f0d87b69adddeb851675741fac3d87930204c70a.

--- js-plot/algebra.cpp
+++ js-plot/algebra.cpp
@@ -1,9 +1,9 @@
 // An example equation
 double equation(double x) {
-  return 2 * x * x * x - 4 * x * x + 6;
+  return x * x * x - x * x + 2;
 }
 
 // Derivative of the above equation
 double derivative(double x) {
-  return 6 * x * x - 8 * x;
+  return 3 * x * x - 2 * x;
 }
