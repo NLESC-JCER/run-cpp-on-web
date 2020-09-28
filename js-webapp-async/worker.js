9c7b9067e413902b548c65ae4ba5e47c8c52a25d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-09-28 13:55:55 +0200

    updated worker.js file to be consistent with the first blog

--- js-webapp-async/worker.js
+++ js-webapp-async/worker.js
@@ -2,11 +2,11 @@ importScripts('newtonraphson.js');
 
 onmessage = function(message) {
   if (message.data.type === 'CALCULATE') {
-    createModule().then((module) => {
-      const epsilon = message.data.payload.epsilon;
-      const finder = new module.NewtonRaphson(epsilon);
-      const guess = message.data.payload.guess;
-      const root = finder.solve(guess);
+    createModule().then(({NewtonRaphson}) => {
+      const tolerance = message.data.payload.tolerance;
+      const finder = new NewtonRaphson(tolerance);
+      const initial_guess = message.data.payload.initial_guess;
+      const root = finder.solve(initial_guess);
       postMessage({
         type: 'RESULT',
         payload: {
