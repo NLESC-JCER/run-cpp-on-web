b3d1746ee35716ce8ca2960871dc2db6f14fb4e3
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-09-28 11:27:49 +0200

    Replace 4 space indents with 2 spaces

--- js-kitchen-sink/app.js
+++ js-kitchen-sink/app.js
@@ -1,92 +1,92 @@
 function iterations2spec(iterations) {
-    // Because the initial guess can be changed now we need to scale equation line to iterations
-    const max_x = Math.max(...iterations.map(d => d.x));
-    const min_x = Math.min(...iterations.map(d => d.x));
-    const equation_line = {
-      "width": 800,
-      "height": 600,
-      "data": {
-        "sequence": {
-          // Draw equation within -4 to 4 window, if iterations are inside of window
-          // otherwise use iterations min to max x
-          "start": min_x > -4 ? -4 : min_x,
-          "stop": max_x < 4 ? 4 : max_x,
-          "step": 0.1, "as": "x"
-        }
-      },
-      "transform": [
-        {
-          "calculate": "2 * datum.x * datum.x * datum.x - 4 * datum.x * datum.x + 6",
-          "as": "y"
-        }
-      ],
-      "mark": "line",
-      "encoding": {
-        "x": { "type": "quantitative", "field": "x" },
-        "y": { "field": "y", "type": "quantitative" },
-        "color": { "value": "#f00" }
+  // Because the initial guess can be changed now we need to scale equation line to iterations
+  const max_x = Math.max(...iterations.map(d => d.x));
+  const min_x = Math.min(...iterations.map(d => d.x));
+  const equation_line = {
+    "width": 800,
+    "height": 600,
+    "data": {
+      "sequence": {
+        // Draw equation within -4 to 4 window, if iterations are inside of window
+        // otherwise use iterations min to max x
+        "start": min_x > -4 ? -4 : min_x,
+        "stop": max_x < 4 ? 4 : max_x,
+        "step": 0.1, "as": "x"
       }
-    };
-    const root_rule = {
-      "data": { "values": [{ "x": -1 }] },
-      "encoding": { "x": { "field": "x", "type": "quantitative" } },
-      "layer": [
-        { "mark": { "type": "rule", "strokeDash": [4, 8] } },
-        { "mark": { "type": "text", "align": "left", "dx": 4, "text": "Root" } }
-      ]
-    };
-    const iterations_scatter = {
-      "title": "2x^3 - 4x^2 + 6 with root",
-      "data": {
-        "values": iterations
+    },
+    "transform": [
+      {
+        "calculate": "2 * datum.x * datum.x * datum.x - 4 * datum.x * datum.x + 6",
+        "as": "y"
+      }
+    ],
+    "mark": "line",
+    "encoding": {
+      "x": { "type": "quantitative", "field": "x" },
+      "y": { "field": "y", "type": "quantitative" },
+      "color": { "value": "#f00" }
+    }
+  };
+  const root_rule = {
+    "data": { "values": [{ "x": -1 }] },
+    "encoding": { "x": { "field": "x", "type": "quantitative" } },
+    "layer": [
+      { "mark": { "type": "rule", "strokeDash": [4, 8] } },
+      { "mark": { "type": "text", "align": "left", "dx": 4, "text": "Root" } }
+    ]
+  };
+  const iterations_scatter = {
+    "title": "2x^3 - 4x^2 + 6 with root",
+    "data": {
+      "values": iterations
+    },
+    "encoding": {
+      "x": { "field": "x", "type": "quantitative" },
+      "y": { "field": "y", "type": "quantitative" },
+      "text": { "field": "index", "type": "nominal" }
+    },
+    "layer": [
+      {
+        "mark": { "type": "circle", "tooltip": { "content": "data" } },
+        "selection": { "grid": { "type": "interval", "bind": "scales" } }
       },
-      "encoding": {
-        "x": { "field": "x", "type": "quantitative" },
-        "y": { "field": "y", "type": "quantitative" },
-        "text": { "field": "index", "type": "nominal" }
-      },
-      "layer": [
-        {
-          "mark": { "type": "circle", "tooltip": { "content": "data" } },
-          "selection": { "grid": { "type": "interval", "bind": "scales" } }
-        },
-        { "mark": { "type": "text", "dy": -10 } }
-      ]
-    };
-    const iteration_vs_y = {
-      "title": "Iterations",
-      "width": 400,
-      "height": 600,
-      "data": {
-        "values": iterations
-      },
-      "encoding": {
-        "x": { "field": "index", "type": "ordinal", "title": "Iteration index" },
-        "y": { "field": "y", "type": "quantitative" }
-      },
-      "mark": {
-        "type": "line",
-        "point": true,
-        // Enable tooltip so on mouseover it shows all data of that iteration
-        "tooltip": { "content": "data" }
-      },
-      // Enable zooming and panning
-      "selection": { "grid": { "type": "interval", "bind": "scales" } }
-    };
+      { "mark": { "type": "text", "dy": -10 } }
+    ]
+  };
+  const iteration_vs_y = {
+    "title": "Iterations",
+    "width": 400,
+    "height": 600,
+    "data": {
+      "values": iterations
+    },
+    "encoding": {
+      "x": { "field": "index", "type": "ordinal", "title": "Iteration index" },
+      "y": { "field": "y", "type": "quantitative" }
+    },
+    "mark": {
+      "type": "line",
+      "point": true,
+      // Enable tooltip so on mouseover it shows all data of that iteration
+      "tooltip": { "content": "data" }
+    },
+    // Enable zooming and panning
+    "selection": { "grid": { "type": "interval", "bind": "scales" } }
+  };
 
-    return {
-      "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-      "hconcat": [
-        {
-          "layer": [
-            equation_line,
-            root_rule,
-            iterations_scatter
-          ]
-        }
-        , iteration_vs_y
-      ]
-    };
+  return {
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "hconcat": [
+      {
+        "layer": [
+          equation_line,
+          root_rule,
+          iterations_scatter
+        ]
+      }
+      , iteration_vs_y
+    ]
+  };
 }
 
 function IterationsPlot({ iterations }) {
